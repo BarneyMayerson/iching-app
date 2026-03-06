e742381b198db2e7bedcee6940597300b4f33937
Barney Mayerson <mr.barney.mayerson@gmail.com>
2026-03-06 16:06:55 +0300

    Upgrade ReadingFactory

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Services\IChingService;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
@@ -15,7 +16,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->singleton(fn ($app): IChingService => new IChingService);
     }
 
     /**
--- database/factories/ReadingFactory.php
+++ database/factories/ReadingFactory.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace Database\Factories;
 
 use App\Models\User;
+use App\Services\IChingService;
 use Illuminate\Database\Eloquent\Factories\Factory;
+use Illuminate\Support\Arr;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Reading>
@@ -19,11 +21,28 @@ class ReadingFactory extends Factory
      */
     public function definition(): array
     {
+        $values = [6, 7, 7, 7, 7, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 9, 9, 9];
+        $coinResults = Arr::random($values, 6);
+
         return [
             'question' => fake()->sentence,
             'user_id' => User::factory(),
-            'coin_results' => [7, 7, 7, 7, 7, 6],
-            'binary' => '111110',
+            'coin_results' => $coinResults,
+            'binary' => app(IChingService::class)->coinResultsToBinary($coinResults),
+
         ];
+    }
+
+    public function stable(): static
+    {
+        return $this->state(function (array $attributes) {
+            $stableValues = [7, 7, 7, 8, 8, 8, 8, 8, 8, 7, 7, 7];
+            $coinResults = Arr::random($stableValues, 6);
+
+            return [
+                'coin_results' => $coinResults,
+                'binary' => app(IChingService::class)->coinResultsToBinary($coinResults),
+            ];
+        });
     }
 }
