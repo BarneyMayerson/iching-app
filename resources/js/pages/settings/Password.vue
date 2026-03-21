db19b18073793cbb8f349de51664d66ae679c01a
Barney Mayerson <mr.barney.mayerson@gmail.com>
2026-03-21 08:21:18 +0300

    Ytanslate settings password

--- resources/js/pages/settings/Password.vue
+++ resources/js/pages/settings/Password.vue
@@ -4,17 +4,20 @@ import InputError from '@/components/InputError.vue';
 import AppLayout from '@/layouts/AppLayout.vue';
 import SettingsLayout from '@/layouts/settings/Layout.vue';
 import { edit } from '@/routes/user-password';
-import { Form, Head } from '@inertiajs/vue3';
+import { Form, Head, usePage } from '@inertiajs/vue3';
 
 import HeadingSmall from '@/components/HeadingSmall.vue';
 import { Button } from '@/components/ui/button';
 import { Input } from '@/components/ui/input';
 import { Label } from '@/components/ui/label';
-import { type BreadcrumbItem } from '@/types';
+import { AppPageProps, type BreadcrumbItem } from '@/types';
+
+const page = usePage<AppPageProps>();
+const __ = (key: string): string => page.props.translations[key] || key;
 
 const breadcrumbItems: BreadcrumbItem[] = [
   {
-    title: 'Password settings',
+    title: __('Password settings'),
     href: edit().url,
   },
 ];
@@ -29,8 +32,12 @@ const breadcrumbItems: BreadcrumbItem[] = [
     <SettingsLayout>
       <div class="space-y-6">
         <HeadingSmall
-          title="Update password"
-          description="Ensure your account is using a long, random password to stay secure"
+          :title="__('Update password')"
+          :description="
+            __(
+              'Ensure your account is using a long, random password to stay secure',
+            )
+          "
         />
 
         <Form
@@ -48,48 +55,50 @@ const breadcrumbItems: BreadcrumbItem[] = [
           v-slot="{ errors, processing, recentlySuccessful }"
         >
           <div class="grid gap-2">
-            <Label for="current_password">Current password</Label>
+            <Label for="current_password">{{ __('Current password') }}</Label>
             <Input
               id="current_password"
               name="current_password"
               type="password"
               class="mt-1 block w-full"
               autocomplete="current-password"
-              placeholder="Current password"
+              :placeholder="__('Current password')"
             />
             <InputError :message="errors.current_password" />
           </div>
 
           <div class="grid gap-2">
-            <Label for="password">New password</Label>
+            <Label for="password">{{ __('New password') }}</Label>
             <Input
               id="password"
               name="password"
               type="password"
               class="mt-1 block w-full"
               autocomplete="new-password"
-              placeholder="New password"
+              :placeholder="__('New password')"
             />
             <InputError :message="errors.password" />
           </div>
 
           <div class="grid gap-2">
-            <Label for="password_confirmation">Confirm password</Label>
+            <Label for="password_confirmation">{{
+              __('Confirm password')
+            }}</Label>
             <Input
               id="password_confirmation"
               name="password_confirmation"
               type="password"
               class="mt-1 block w-full"
               autocomplete="new-password"
-              placeholder="Confirm password"
+              :placeholder="__('Confirm password')"
             />
             <InputError :message="errors.password_confirmation" />
           </div>
 
           <div class="flex items-center gap-4">
-            <Button :disabled="processing" data-test="update-password-button"
-              >Save password</Button
-            >
+            <Button :disabled="processing" data-test="update-password-button">
+              {{ __('Save password') }}
+            </Button>
 
             <Transition
               enter-active-class="transition ease-in-out"
@@ -98,7 +107,7 @@ const breadcrumbItems: BreadcrumbItem[] = [
               leave-to-class="opacity-0"
             >
               <p v-show="recentlySuccessful" class="text-sm text-neutral-600">
-                Saved.
+                {{ __('Saved.') }}
               </p>
             </Transition>
           </div>
