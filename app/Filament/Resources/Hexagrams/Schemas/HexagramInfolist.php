5a84243074aaf7843af134e1f75dceb00d1c9cd2
Barney Mayerson <mr.barney.mayerson@gmail.com>
2026-04-02 15:24:03 +0300

    View hexagram filament resource

--- app/Filament/Resources/Hexagrams/Schemas/HexagramInfolist.php
+++ app/Filament/Resources/Hexagrams/Schemas/HexagramInfolist.php
@@ -1,9 +1,14 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\Filament\Resources\Hexagrams\Schemas;
 
 use Filament\Infolists\Components\TextEntry;
+use Filament\Schemas\Components\Section;
 use Filament\Schemas\Schema;
+use Filament\Support\Enums\FontWeight;
+use Filament\Support\Enums\Size;
 
 class HexagramInfolist
 {
@@ -11,16 +16,62 @@ class HexagramInfolist
     {
         return $schema
             ->components([
-                TextEntry::make('number')
-                    ->numeric(),
-                TextEntry::make('chinese_name'),
-                TextEntry::make('pinyin_name'),
-                TextEntry::make('character'),
-                TextEntry::make('upperTrigram.id')
-                    ->label('Upper trigram'),
-                TextEntry::make('lowerTrigram.id')
-                    ->label('Lower trigram'),
-                TextEntry::make('binary'),
+                Section::make(__('General Information'))
+                    ->schema([
+                        TextEntry::make('number')
+                            ->label('#')
+                            ->inlineLabel(),
+
+                        TextEntry::make('character')
+                            ->label(__('Character'))
+                            ->extraAttributes([
+                                'style' => 'font-size: 3rem; line-height: 1; font-family: serif; padding-top: 0.5rem; padding-bottom: 0.5rem;',
+                            ])
+                            ->inlineLabel(),
+
+                        TextEntry::make('names')
+                            ->label(__('Name'))
+                            ->size(Size::Large->value)
+                            ->weight(FontWeight::Bold)
+                            ->inlineLabel(),
+
+                        TextEntry::make('binary')
+                            ->label(__('Binary'))
+                            ->fontFamily('mono')
+                            ->inlineLabel(),
+                    ])
+                    ->columnSpanFull(),
+
+                Section::make(__('Structure'))
+                    ->schema([
+                        TextEntry::make('upperTrigram.names')
+                            ->label(__('Upper Trigram'))
+                            ->inlineLabel(),
+
+                        TextEntry::make('lowerTrigram.names')
+                            ->label(__('Lower Trigram'))
+                            ->inlineLabel(),
+                    ])
+                    ->columns(2)
+                    ->columnSpanFull(),
+
+                Section::make(__('Interpretation'))
+                    ->schema([
+                        TextEntry::make('chinese_name')
+                            ->label(__('Chinese Name'))
+                            ->inlineLabel(),
+
+                        TextEntry::make('pinyin_name')
+                            ->label(__('Pinyin'))
+                            ->inlineLabel(),
+
+                        TextEntry::make('judgment')
+                            ->label(__('Judgment'))
+                            ->html()
+                            ->columnSpanFull(),
+                    ])
+                    ->columns(2)
+                    ->columnSpanFull(),
             ]);
     }
 }
