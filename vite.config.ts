23e203c1b7738a3c5aaef368e8e534b32f812b6c
Barney Mayerson <mr.barney.mayerson@gmail.com>
2026-04-17 14:32:28 +0300

    Fix npm build

--- vite.config.ts
+++ vite.config.ts
@@ -26,10 +26,7 @@ export default defineConfig({
   },
   plugins: [
     laravel({
-      input: [
-        'resources/js/app.ts',
-        'resources/views/filament/hooks/langguage-switcher.blade.php',
-      ],
+      input: ['resources/js/app.ts'],
       ssr: 'resources/js/ssr.ts',
       refresh: true,
     }),
